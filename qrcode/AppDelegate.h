c627727da132677503d6234649fa03918e19e502
Shoota Kumano <shoota.dev@gmail.com>
2015-03-20 22:10:01 +0900

    Initial Commit

---
+++ qrcode/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  qrcode
+//
+//  Created by 熊野修太 on 2015/03/20.
+//  Copyright (c) 2015年 anaguma.org. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ qrcode/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  qrcode
+//
+//  Created by 熊野修太 on 2015/03/20.
+//  Copyright (c) 2015年 anaguma.org. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
