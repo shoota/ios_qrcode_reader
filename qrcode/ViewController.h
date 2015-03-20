c1c3a4028d4c3b88e084601219e3b11700d1df0c
Shoota Kumano <shoota.dev@gmail.com>
2015-03-20 22:59:20 +0900

    minimum QR code detection

--- qrcode/ViewController.h
+++ qrcode/ViewController.h
@@ -7,8 +7,15 @@
 //
 
 #import <UIKit/UIKit.h>
+#import <AVFoundation/AVFoundation.h>
 
-@interface ViewController : UIViewController
+@interface ViewController : UIViewController <AVCaptureMetadataOutputObjectsDelegate>
+
+@property (strong, nonatomic) AVCaptureDevice* device;
+@property (strong, nonatomic) AVCaptureDeviceInput* input;
+@property (strong, nonatomic) AVCaptureMetadataOutput* output;
+@property (strong, nonatomic) AVCaptureSession* session;
+@property (strong, nonatomic) AVCaptureVideoPreviewLayer* preview;
 
 
 @end
--- qrcode/ViewController.m
+++ qrcode/ViewController.m
@@ -16,12 +16,67 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    
+    // Device
+    self.device = [AVCaptureDevice defaultDeviceWithMediaType:AVMediaTypeVideo];
+    
+    // session
+    self.session = [[AVCaptureSession alloc] init];
+    
+    // Input
+    self.input = [AVCaptureDeviceInput deviceInputWithDevice:self.device error:nil];
+    if (self.input) {
+        [self.session addInput:self.input];
+    } else {
+        NSLog(@"error");
+    }
+
+    
+    // Output
+    self.output = [[AVCaptureMetadataOutput alloc] init];
+    [self.session addOutput:self.output];
+    [self.output setMetadataObjectsDelegate:self queue:dispatch_get_main_queue()];
+    [self.output setMetadataObjectTypes:@[AVMetadataObjectTypeQRCode, AVMetadataObjectTypeEAN13Code]];
+    NSLog(@"%@", [self.output availableMetadataObjectTypes]);
+    
+    
+    // Preview
+   AVCaptureVideoPreviewLayer *preview = [AVCaptureVideoPreviewLayer layerWithSession:self.session];
+    preview.videoGravity = AVLayerVideoGravityResizeAspectFill;
+    preview.frame = CGRectMake(0, 0, self.view.frame.size.width, self.view.frame.size.height);
+    [self.view.layer insertSublayer:preview atIndex:0];
+    
+    // Start
+    [self.session startRunning];
 }
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
+
+- (void)captureOutput:(AVCaptureOutput *)captureOutput didOutputMetadataObjects:(NSArray *)metadataObjects
+       fromConnection:(AVCaptureConnection *)connection
+{
+    // 認識されたメタデータは複数存在することもあるので、１つずつ調べる
+    for (AVMetadataObject *data in metadataObjects) {
+        // 一次元・二次元コード以外は無視する
+        // ※人物顔の識別結果だった場合など
+        if (![data isKindOfClass:[AVMetadataMachineReadableCodeObject class]]) continue;
+        
+        // コード内の文字列を取得
+        NSString *strValue = [(AVMetadataMachineReadableCodeObject *)data stringValue];
+        
+        // 何のタイプとして認識されたかを確認
+        if ([data.type isEqualToString:AVMetadataObjectTypeQRCode]) {
+            // QRコードの場合、URLとしてmobileSafariを開く
+            NSURL *url = [NSURL URLWithString:strValue];
+            if ([[UIApplication sharedApplication] canOpenURL:url]) {
+                [[UIApplication sharedApplication] openURL:url];
+            }
+        }
+    }
+}
+
 
 @end
